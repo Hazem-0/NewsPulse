1a277d0c74c8b5cf36efc663a088ba69cb9e4bab
hazemkora660@gmail.com <hazemkora660@gmail.com>
2026-06-24 23:43:56 +0300

    Remove hardcoded API key from NetworkConfig
    - Clear the value of the `API_KEY` constant in `NetworkConfig.kt`.
    - Add a comment placeholder to the `API_KEY` property.

--- sharedLogic/src/commonMain/kotlin/com/darkzoom/newspulse/data/remote/utils/NetworkConfig.kt
+++ sharedLogic/src/commonMain/kotlin/com/darkzoom/newspulse/data/remote/utils/NetworkConfig.kt
@@ -2,5 +2,5 @@ package com.darkzoom.newspulse.data.remote.utils
 
 object NetworkConfig {
     const val BASE_URL = "https://newsapi.org/v2"
-    const val API_KEY = "REMOVED_API_KEY"
+    const val API_KEY = "" // ignored the key
 }
