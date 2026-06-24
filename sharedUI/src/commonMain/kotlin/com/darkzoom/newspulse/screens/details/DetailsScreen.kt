2a35175535f26bfaf5af4f002fabae7567b37db1
hazemkora660@gmail.com <hazemkora660@gmail.com>
2026-06-24 23:34:02 +0300

    Move DetailsScreen to screens package
    - Relocate `DetailsScreen.kt` from `com.darkzoom.newspulse.view.details` to `com.darkzoom.newspulse.screens.details`.
    - Update the package declaration within `DetailsScreen.kt` to reflect the new directory structure.

--- sharedUI/src/commonMain/kotlin/com/darkzoom/newspulse/screens/details/DetailsScreen.kt
+++ sharedUI/src/commonMain/kotlin/com/darkzoom/newspulse/screens/details/DetailsScreen.kt
@@ -1,4 +1,4 @@
-package com.darkzoom.newspulse.view.details
+package com.darkzoom.newspulse.screens.details
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
